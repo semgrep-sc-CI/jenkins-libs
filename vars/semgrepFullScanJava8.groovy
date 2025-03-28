b5b56996796d844d3bfa2e7711a34e7c90a1c01a
semgrep-shichao <shi@semgrep.com>
2025-03-28 16:44:16 +0800

    Create semgrepFullScanJava8.groovy

---
+++ vars/semgrepFullScanJava8.groovy
@@ -0,0 +1,13 @@
+def call() {
+  sh '''docker pull semgrep/semgrep && \
+            docker run \
+            -e SEMGREP_APP_TOKEN=$SEMGREP_APP_TOKEN \
+            -v "$(pwd):$(pwd)" --workdir $(pwd) \
+            semgrep/semgrep  \
+            sh \
+            -c "echo "https://dl-cdn.alpinelinux.org/alpine/edge/testing">>/etc/apk/repositories && \
+            apk update && \
+            apk add openjdk8 maven && \
+            semgrep ci --allow-local-builds"
+     '''
+}
