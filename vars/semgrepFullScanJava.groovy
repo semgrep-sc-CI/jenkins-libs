156a9661d2a6800c620755db75f8dd2940fbc300
semgrep-shichao <shi@semgrep.com>
2025-03-28 16:25:39 +0800

    Update semgrepFullScanJava.groovy

--- vars/semgrepFullScanJava.groovy
+++ vars/semgrepFullScanJava.groovy
@@ -3,5 +3,10 @@ def call() {
             docker run \
             -e SEMGREP_APP_TOKEN=$SEMGREP_APP_TOKEN \
             -v "$(pwd):$(pwd)" --workdir $(pwd) \
-            semgrep/semgrep  apk update && apk add openjdk23-jdk maven && semgrep ci --allow-local-builds '''
+            semgrep/semgrep  \
+            echo "https://dl-cdn.alpinelinux.org/alpine/edge/testing" >>/etc/apk/repositories
+            apk update && \
+            apk add openjdk23-jdk maven && \
+            semgrep ci --allow-local-builds 
+     '''
 }
